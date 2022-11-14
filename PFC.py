d6bf0b000b7015c4039277d48198332f43e6ffbf
Samantha <sschneibel@gaming.tech>
2022-11-14 10:33:08 +0100

    traduire en pyhton

--- PFC.py
+++ PFC.py
@@ -1,79 +1,56 @@
-#DEBUT
+from random import randint
 
-#on admet qu'une fonction random existe et renvoie un chiffre aléatoire entre 1 et 3
-#on admet qu'une fonction input existe et demande à l'utilisateur de rentrer une chaîne de caractères et la retourne
-#définir un fonction pierreFeuilleCiseaux
-    #créer une variable playerPoint et lui associer 0
-    #créer une variable ordiPoint et lui associer 0
-    #créer une variable score et lui associer la concaténation nickname + ":" + str(playerPoint) + "Ordinateur : " + str(ordiPoint)
-    #créer une variable nickname et lui associer le retour de l'exécution de la fonction input("Entrez votre pseudo : ")
-    #créer une variable game et lui associer le retour de l'exécution de la fonction input("{nickname}, voulez-vous jouer ? : ")
-    #otherGame est égal à vrai
-        #si game est égal à "yes"
-            #alors game est vrai
-        #si game est égal à "no"
-            #alors game est faux
-        #tant que otherGame est vrai
-        #tant que game est vrai
-            #afficher "Chi-fou-mi"
-                #créer une variable playerChoice et lui associer le retour de l'exécution de la fonction input("(P)ierre/(F)euille/(C)iseaux")
-                #créer une variable ordiChoice et lui associer le retour de l'exécution de la fonction random
-                    #si playerChoice est égal à "P" ou "p" ET ordiChoice est égal à 1
-                        #alors afficher "Pierre VS Pierre"
-                        #afficher "Egalité"
-                        #afficher score
-                    #sinon si playerChoice est égal à "F" ou "f" ET ordiChoice est égal à 2
-                        #alors afficher "Feuille VS Feuille"
-                        #afficher "Egalité"
-                        #afficher score
-                    #sinon si playerChoice est égal à "C" ou "c" ET ordiChoice est égal à 3
-                        #alors afficher "Ciseaux VS Ciseaux"
-                        #afficher "Egalité"
-                        #afficher score
-                    #sinon si playerChoice est égal à "P" ou "p" ET ordiChoice est égal à 3
-                        #alors afficher "Pierre VS Ciseaux"
-                        #incrémenter playerPoint de 1
-                        #afficher "Gagné"
-                        #afficher score
-                    #sinon si playerChoice est égal à "F" ou "f" ET ordiChoice est égal à 1
-                        #alors afficher "Feuille VS Pierre"
-                        #incrémenter playerPoint de 1
-                        #afficher "Gagné"
-                        #afficher score
-                    #sinon si playerChoice est égal à "C" ou "c" ET ordiChoice est égal à 2
-                        #alors afficher "Ciseaux VS Feuille"
-                        #incrémenter playerPoint de 1
-                        #afficher "Gagné"
-                        #afficher score
-                    #sinon si playerChoice est égal à "P" ou "p" ET ordiChoice est égal à 2
-                        #alors afficher "Pierre VS Feuille"
-                        #incrémenter ordiPoint de 1
-                        #afficher "Perdu"
-                        #afficher score
-                    #sinon si playerChoice est égal à "F" ou "f" ET ordiChoice est égal à 3
-                        #alors afficher "Feuille VS Ciseaux"
-                        #incrémenter ordiPoint de 1
-                        #afficher "Perdu"
-                        #afficher score
-                    #sinon si playerChoice est égal à "C" ou "c" ET ordiChoice est égal à 1
-                        #alors afficher "Ciseaux VS Pierre"
-                        #incrémenter ordiPoint de 1
-                        #afficher "Perdu"
-                        #afficher score
-                    #si playerPoint est égal à 3
-                        #alors afficher nickname + "vous avez gagné la partie ! :)"
-                        #casser la boucle
-                    #sinon si ordiPoint est égal à 3
-                        #alors afficher nickname + "vous avez perdu la partie ! :("
-                        #casser la boucle
-        #créer une variable otherGame et lui associer le retour de l'exécution de la fonction input("{nickname}, voulez-vous re jouer ? : ")
-            #si otherGame est égal à "yes"
-                #alors otherGame est vrai
-                #afficher "C'est parti !"
-            #si otherGame est égal à "no"
-                #alors otherGame est faux
-                #afficher "A bientôt nickname !"
-                #casser la boucle
-#exécuter la fonction pierreFeuilleCiseaux()
+def pierreFeuilleCiseaux():
+    nickname = input("Entrez votre pseudo : ")
+    playerPoint = 0
+    computerPoint = 0
+    
+    game = input(nickname + ", voulez-vous jouer ?")
+    if game == "yes":
+        game = True
+    elif game == "no":
+        game = False
 
-#FIN
+    while game == True:
+        print("Chi-fou-mi")
+
+        computerChoice = randint(1, 3)
+        if computerChoice == 1:
+            computerChoice = "Pierre"
+        elif computerChoice == 2:
+            computerChoice = "Feuille"
+        else: computerChoice = "Ciseaux"
+
+        playerChoice = input("(P)ierre/(F)euille/(C)iseaux")
+        if playerChoice == "P" or "p":
+            playerChoice = "Pierre"
+        elif playerChoice == "F" or "f":
+            playerChoice = "Feuille"
+        elif playerChoice == "C" or "c":
+            playerChoice = "Ciseaux"
+
+        if playerChoice == computerChoice :
+            print(playerChoice + " VS " + computerChoice)
+            print("Egalité")
+            print(nickname + ":" + str(playerPoint) + "Ordinateur :" + str(computerPoint))
+
+        elif playerChoice == "Pierre" and computerChoice == "Ciseaux" or (playerChoice == "Feuille" and computerChoice == "Pierre") or(playerChoice == "Ciseaux" and computerChoice == "Feuille"):
+            print(playerChoice + " VS " + computerChoice)
+            print("Gagné")
+            playerPoint = playerPoint + 1
+            print(nickname + ":" + str(playerPoint) + "Ordinateur :" + str(computerPoint))
+        
+        elif playerChoice == "Pierre" and computerChoice == "Feuille" or (playerChoice == "Feuille" and computerChoice == "Ciseaux") or(playerChoice == "Ciseaux" and computerChoice == "Pierre"):
+            print(playerChoice + " VS " + computerChoice)
+            print("Perdu")
+            computerPoint = computerPoint + 1
+            print(nickname + ":" + str(playerPoint) + "Ordinateur :" + str(computerPoint))
+
+        if playerPoint == 3:
+            print(nickname + ", vous avez gagné la partie ! :)")
+            break
+        elif computerPoint == 3:
+            print(nickname + ", vous avez perdu la partie ! :(")
+            break
+
+pierreFeuilleCiseaux()
